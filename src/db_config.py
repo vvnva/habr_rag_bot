cced34bd34bd098057c26a6b322e0222e7f5489b
Artemev Nikita <nikita-artemev-03@mail.ru>
2024-12-29 23:55:42 +0300

    fixed structure v2.0

--- src/db_config.py
+++
@@ -1,7 +0,0 @@
-db_config = {
-    'host': 'localhost',          # Адрес сервера базы данных
-    'port': '5432',               # Порт PostgreSQL, по умолчанию 5432
-    'dbname': 'habr',           # Имя базы данных
-    'user': 'postgres',           # Имя пользователя
-    'password': '1'   # Пароль пользователя
-}
